2fc3d8548e2cba8840555c34195e8fed237f4ad2
wilhelm17 <wilhelm.jochim.98@gmail.com>
2015-02-01 13:28:50 +0100

    test

---
+++ MyGame/src/Game/Screen.java
@@ -0,0 +1,32 @@
+package Game;
+
+public class Screen {
+
+	public int width, height;
+	public int[] pixels;
+
+	public int xOffset, yOffset;
+
+	public Screen(int width, int height) {
+
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+
+	}
+
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0xB0B0B0;
+		}
+	}
+
+	public void renderPlayer(int color, int xspawn, int yspawn, int xOffset,
+			int yOffset) {
+		for (int xb = 0; xb < 8; xb++) {
+			for (int yb = 0; yb < 8; yb++) {
+				pixels[xspawn + xOffset + xb + (yspawn + yOffset + yb) * width] = color;
+			}
+		}
+	}
+}
