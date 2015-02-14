7bdef0867f76283d493ed0039368a1da57203364
wilhelm17 <wilhelm.jochim.98@gmail.com>
2015-02-14 13:26:40 +0100

    1

--- MyGame/src/Entities/Player.java
+++ MyGame/src/Entities/Player.java
@@ -1,5 +1,7 @@
 package Entities;
 
+import java.util.ArrayList;
+
 import Game.Screen;
 import Input.Keyboard;
 
@@ -12,6 +14,7 @@ public class Player {
 	double alpha = 0;
 	double beta = 0;
 	public boolean moving = true;
+	public ArrayList<Integer> a = new ArrayList<Integer>();
 
 	public Player(int color, Keyboard key, Screen screen, int xspawn, int yspawn) {
 		this.key = key;
@@ -19,7 +22,9 @@ public class Player {
 		this.screen = screen;
 		this.xOffset += xspawn;
 		this.yOffset += yspawn;
-
+		for(int i = 0; i <= 640;i++){
+			a.add(0);
+		}
 	}
 
 	public void update() {
--- MyGame/src/Game/Screen.java
+++ MyGame/src/Game/Screen.java
@@ -1,18 +1,19 @@
 package Game;
 
+
 import Entities.Player;
 
 public class Screen {
 
 	public int width, height;
 	public int[] pixels;
+	int aIndex = 0;
 
 	public Screen(int width, int height) {
 
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
-
 	}
 
 	public void clear() {
@@ -28,7 +29,7 @@ public class Screen {
 		for (int i = (height - 1) * width; i < (height - 0) * width; i++) {
 			pixels[i] = 0xffffff;
 		}
-		
+
 		for (int i = 0; i < pixels.length; i += width) {
 			pixels[i] = 0xffffff;
 		}
@@ -41,11 +42,20 @@ public class Screen {
 
 		for (int xb = 0; xb < 8; xb++) {
 			for (int yb = 0; yb < 8; yb++) {
-				if (pixels[x + xb + (y + yb) * width] != 0x000000
-						&& pixels[x + xb + (y + yb) * width] != color) {
-					p.moving = false;
+				if (pixels[x + xb + (y + yb) * width] != 0x000000) {
+					if (pixels[x + xb + (y + yb) * width] != color) {
+						p.moving = false;
+					} else if (pixels[x + xb + (y + yb) * width] == color
+							&& !p.a.contains(x + xb + (y + yb) * width)) {
+						p.moving = false;
+					}
 				}
 				pixels[x + xb + (y + yb) * width] = color;
+				p.a.set(aIndex, x + xb + (y + yb) * width);
+				aIndex++;
+				if (aIndex > 640) {
+					aIndex = 0;
+				}
 			}
 		}
 	}
